161aef4b139c02d6d56c2d3f59e228e2536b4cdb
kw <2818815189@qq.com>
2020-07-08 19:45:25 +0800

    颜色渐变

--- app/src/main/java/com/example/myapplication/controller/MyGame.java
+++ app/src/main/java/com/example/myapplication/controller/MyGame.java
@@ -3,6 +3,7 @@ package com.example.myapplication.controller;
 import android.opengl.GLES20;
 
 import com.example.myapplication.core.Game;
+import com.example.myapplication.shape.ChangerColorTriangleMatrix;
 import com.example.myapplication.shape.Triangle;
 import com.example.myapplication.shape.TriangleMatrix;
 
@@ -10,24 +11,28 @@ public class MyGame extends Game {
     //显示三角形
 //    private Triangle triangle;
     //显示等腰三角形
-    private TriangleMatrix triangleMatrix ;
-
+//    private TriangleMatrix triangleMatrix ;
+//  变化颜色
+    private ChangerColorTriangleMatrix matrix ;
     public MyGame (){
 //        triangle = new Triangle();
-        triangleMatrix = new TriangleMatrix();
+//        triangleMatrix = new TriangleMatrix();
+        matrix = new ChangerColorTriangleMatrix();
     }
     @Override
     public void create() {
 //        triangle.create();
-        triangleMatrix.create();
+//        triangleMatrix.create();
+        matrix.create();
     }
 
 
     @Override
     public void render() {
-        GLES20.glClearColor(0,0,0,0);
+        GLES20.glClearColor(0.5F,0.5F,0.5F,1);
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        triangleMatrix.render();
+//        triangleMatrix.render();
+        matrix.render();
     }
 
 
@@ -38,6 +43,7 @@ public class MyGame extends Game {
 
     @Override
     public void surfaceChanage(int width, int height) {
-        triangleMatrix.surfaceChange(width,height);
+//        triangleMatrix.surfaceChange(width,height);
+        matrix.surfaceChange(width,height);
     }
 }
