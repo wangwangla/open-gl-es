f1c64a600a67932c68b33162f6b99c27bd73f140
wangwangla <2818815189@qq.com>
2021-03-31 23:15:30 +0800

    矩阵变换  确实是没有想明白

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,20 +22,20 @@ public class MainActivity extends Activity {
         graphics = new AndroidGraphics(this);
         setContentView(graphics.getView());
 
-        EGLDisplay display = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
-        if (display == EGL14.EGL_NO_DISPLAY) {
-            System.out.println("unable");
-        }
-        int arr[] = new int[1];
-        int arr2[] = new int[1];
-        if (!EGL14.eglInitialize(display,arr,0,arr2,0)){
-            System.out.println("unable chu shi hua");
-        }
-        EGLConfig con[] = new EGLConfig[10];
-        int xx [] = new int[1];
-        boolean b = EGL14.eglGetConfigs(display, con, 0, 10, xx, 0);
-        System.out.println(b);
-        System.out.println("=>>>......");
+//        EGLDisplay display = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
+//        if (display == EGL14.EGL_NO_DISPLAY) {
+//            System.out.println("unable");
+//        }
+//        int arr[] = new int[1];
+//        int arr2[] = new int[1];
+//        if (!EGL14.eglInitialize(display,arr,0,arr2,0)){
+//            System.out.println("unable chu shi hua");
+//        }
+//        EGLConfig con[] = new EGLConfig[10];
+//        int xx [] = new int[1];
+//        boolean b = EGL14.eglGetConfigs(display, con, 0, 10, xx, 0);
+//        System.out.println(b);
+//        System.out.println("=>>>......");
     }
 
 
